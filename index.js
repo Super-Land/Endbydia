b9848659f3e11604a06c71e31af6f99084cdade6
SuperLand <mario_gabriell_jesus@hotmail.com>
2022-08-06 11:18:33 -0400

    commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const buscadorImagenes = document.querySelector(".buscadorImagenes");
+if (buscadorImagenes === "el texto q cuando ponga el q usa la página ponga la imagen") {
+  document.write("el buscador si sirve");
+}
